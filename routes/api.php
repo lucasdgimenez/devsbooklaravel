ff1cbb5da42a6bb31e5f1bd9ca1d0932a9012f57
lucasdgimenez <lucas_d360@hotmail.com>
2023-03-14 21:15:36 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('/ping', function() {
+    return ['pong'=>true];
+});
+/*
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+*/
+Route::get('/401', 'AuthController@unauthorized')->name('login');
+
+Route::post('/auth/login', 'AuthController@login');
+Route::post('/auth/logout', 'AuthController@logout');
+Route::post('/auth/refresh', 'AuthController@refresh');
+
+Route::post('/user', 'AuthController@create');
+Route::put('/user', 'AuthController@update');
+Route::post('/user/avatar', 'UserController@updateAvatar');
+Route::post('/user/cover', 'UserController@updateCover');
+
+Route::get('/feed', 'FeedController@read');
+Route::get('/user/feed', 'FeedController@userFeed');
+Route::get('/user/{id}/feed', 'FeedController@userFeed');
+
+Route::get('/user', 'UserController@read');
+Route::get('/user/{id}', 'UserController@read');
+
+Route::post('/feed', 'FeedController@create');
+
+Route::post('/post/{id}/like', 'PostController@like');
+Route::post('/post/{id}/comment', 'PostController@comment');
+
+Route::get('/search', 'SearchController@search');
